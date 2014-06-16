029dd797a8bea8c16bee2be3004272b69f62f250
Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
2014-06-16 17:35:00 +0300

    Converting indenting from spaces to tabs
    - Renamed some connection structs from c to con as c is usually
      reserved for char variables
    
    Signed-off-by: Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>

--- src/client/attach_conn.c
+++ src/client/attach_conn.c
@@ -1,9 +1,3 @@
-#include "common/common.h"
-#include "common/iirc.pb-c.h"
-#include "common/epoll_cont.h"
-#include "client/attach_conn.h"
-#include "client/screen_conn.h"
-
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <string.h>
@@ -12,69 +6,97 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "common/common.h"
+#include "common/iirc.pb-c.h"
+#include "common/epoll_cont.h"
+#include "client/attach_conn.h"
+#include "client/screen_conn.h"
 
-struct conn* attach_conn_add(struct epoll_cont* e, const char* hn) {
-  struct conn* c = NULL;
-  int to[2];
-  int from[2];
-  if (pipe(to) < 0) die("pipe");
-  if (pipe(from) < 0) die("pipe");
+struct conn* attach_conn_add(struct epoll_cont* e, const char* hn)
+{
+	struct conn* con = NULL;
+	int to[2];
+	int from[2];
+	pid_t pid;
 
-  pid_t pid = fork();
-  if (pid < 0) die("fork");
-  if (pid == 0) {
-    attach_conn_exec(hn, to, from);
-  } else {
-    if (close(to[0]) < 0) die("close");
-    if (close(from[1]) < 0) die("close");
-    c = epoll_cont_add(e, from[0], to[1]);
-    if (!c) die2("no connection slot for child pipe");
-    c->cbs[EV_WRITE] = conn_write;
-    c->cbs[EV_READ] = conn_read;
-    c->cbs[EV_AFTER_READ] = attach_conn_after_read;
-    c->cbs[EV_CLOSE] = attach_conn_close;
+	if (pipe(to) < 0) die("pipe");
+	if (pipe(from) < 0) die("pipe");
+
+	pid = fork();
+	if (pid < 0)
+		die("fork");
+	if (pid == 0) {
+		attach_conn_exec(hn, to, from);
+	} else {
+	if (close(to[0]) < 0)
+		die("close");
+	if (close(from[1]) < 0)
+		die("close");
+
+    con = epoll_cont_add(e, from[0], to[1]);
+    if (!con)
+	    die2("no connection slot for child pipe");
+    con->cbs[EV_WRITE] = conn_write;
+    con->cbs[EV_READ] = conn_read;
+    con->cbs[EV_AFTER_READ] = attach_conn_after_read;
+    con->cbs[EV_CLOSE] = attach_conn_close;
   }
-  return c;
+
+  return con;
 }
 
-int attach_conn_close(struct epoll_cont* e, struct conn* c, struct event* ev) {
-  conn_close(e, c, ev);
-  struct screen* s = e->ptr;
-  if (s) s->attach_conn = NULL;
-  return 0;
+int attach_conn_close(struct epoll_cont* e, struct conn* c, struct event* ev)
+{
+	struct screen* s;
+
+	conn_close(e, c, ev);
+	s = e->ptr;
+	if (s)
+		s->attach_conn = NULL;
+
+	return 0;
 }
 
-void attach_conn_exec(const char* host, int to[2], int from[2]) {
+void attach_conn_exec(const char* host, int to[2], int from[2])
+{
   const char cmd[] = "iirc-attach";
-  int null = open("/dev/null", O_WRONLY);
-  if (null < 0) die("open");
-  if (dup2(null, STDERR_FILENO) < 0) die("dup2");
-  if (dup2(to[0], STDIN_FILENO) < 0) die("dup2");
-  if (dup2(from[1], STDOUT_FILENO) < 0) die("dup2");
-  if (host) {
-    if (execlp("ssh", "ssh", host, cmd, (char*)NULL) < 0)
-      die("execlp");
-  } else {
-    if (execlp(cmd, cmd, (char*)NULL) < 0)
-      die("execlp");
-  }
+  int devnull = open("/dev/null", O_WRONLY);
+
+	if (devnull < 0)
+		die("open");
+	if (dup2(devnull, STDERR_FILENO) < 0)
+		die("dup2");
+	if (dup2(to[0], STDIN_FILENO) < 0)
+		die("dup2");
+	if (dup2(from[1], STDOUT_FILENO) < 0)
+		die("dup2");
+	if (host) {
+		if (execlp("ssh", "ssh", host, cmd, (char*)NULL) < 0)
+			die("execlp");
+	} else {
+		if (execlp(cmd, cmd, (char*)NULL) < 0)
+			die("execlp");
+	}
 }
 
-int attach_conn_after_read(struct epoll_cont* e, struct conn* c,
+int attach_conn_after_read(struct epoll_cont* e, struct conn* con,
     struct event* ev)
 {
-  while(c->in_pos >= (ssize_t)sizeof(uint16_t)) {
-    uint16_t len;
-    memcpy(&len, c->in_buf, sizeof(uint16_t));
-    len = ntohs(len);
-    if (c->in_pos+sizeof(uint16_t) < len) break;
-    AMessage* m = amessage__unpack(NULL, len, (uint8_t*)&c->in_buf[2]);
-    if (m && m->type == MESSAGE_TYPE__ROW) {
-      screen_add_line(e->ptr, m);
-    }
-    amessage__free_unpacked(m, NULL);
-    c->in_pos -= len + sizeof(uint16_t);
-    memmove(c->in_buf, &c->in_buf[len + sizeof(uint16_t)], c->in_pos);
-  }
-  return 1;
+	while(con->in_pos >= (ssize_t)sizeof(uint16_t)) {
+		uint16_t len;
+
+		memcpy(&len, con->in_buf, sizeof(uint16_t));
+		len = ntohs(len);
+		if (con->in_pos + sizeof(uint16_t) < len)
+			break;
+		AMessage* m = amessage__unpack(NULL, len, (uint8_t*)&con->in_buf[2]);
+		if (m && m->type == MESSAGE_TYPE__ROW) {
+			screen_add_line(e->ptr, m);
+		}
+		amessage__free_unpacked(m, NULL);
+		con->in_pos -= len + sizeof(uint16_t);
+		memmove(con->in_buf, &con->in_buf[len + sizeof(uint16_t)], con->in_pos);
+	}
+
+	return 1;
 }
--- src/client/main.c
+++ src/client/main.c
@@ -4,19 +4,21 @@
 #include "client/attach_conn.h"
 #include "client/screen_conn.h"
 
-int main(int argc, char** argv) {
-  struct screen scr;
-  screen_init(&scr);
-  screen_draw(&scr);
+int main(int argc, char** argv)
+{
+	struct screen scr;
+	struct epoll_cont e;
 
-  struct epoll_cont e;
-  epoll_cont_init(&e);
-  e.ptr = &scr;
+	screen_init(&scr);
+	screen_draw(&scr);
 
-  scr.attach_conn = attach_conn_add(&e, argv[1]);
-  screen_conn_add(&e);
+	epoll_cont_init(&e);
+	e.ptr = &scr;
 
-  epoll_cont_serve(&e);
-  epoll_cont_destroy(&e);
-  screen_destroy(&scr);
+	scr.attach_conn = attach_conn_add(&e, argv[1]);
+	screen_conn_add(&e);
+
+	epoll_cont_serve(&e);
+	epoll_cont_destroy(&e);
+	screen_destroy(&scr);
 }
